b06787cf9b113ae9042dba2190dbd107979327ff
Izydorek <maciej.izydorek@emc.com>
2020-02-08 16:42:54 +0000

    Added comments to program

--- collatz.py
+++ collatz.py
@@ -1,17 +1,17 @@
 # Maciej Izydorek
-# Collatz conjecture
+# Collatz conjecture 
 
 n = int(input("Please enter a positive number: "))
 
 print(int(n))
-while n == 1:
-    n = 3 * n + 1
+while n == 1:                   # validates if number is equal to one 
+    n = 3 * n + 1               
     print(int(n))
 else:
-    while n != 1:
-        if n % 2 == 0: 
+    while n != 1:               # statement that validates number from 2 up
+        if n % 2 == 0:          # checks if it is even number
             n = n / 2
             print(int(n))
-        else:
+        else:                   # checks if it is odd number
             n = 3 * n + 1
             print(int(n))
