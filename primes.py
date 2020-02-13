ea4625b49c5a84c02c355ca8d79a1678d33639d3
Izydorek <maciej.izydorek@emc.com>
2020-02-13 17:09:37 +0000

    Simplifying primes code by adding else statement to for loop

--- primes.py
+++ primes.py
@@ -4,16 +4,14 @@
 P = []
 # loops through numbers from 0 to 100 and checking if it is prime
 for i in range(2, 100):
-    isprime = True
     # loops through values of P 
     for j in P:
         # see if j divides i
         if i % j == 0:
-            # If it does, i is not a prime so exit loop and indicate it's not prime
-            isprime = False
+            # If it does, i is not a prime so exit loop
             break
-    # If i is prime, then append to P 
-    if isprime:
+    # else i is prime, then append to P 
+    else:
         P.append(i)
 #print out list of primes
 print(P)
